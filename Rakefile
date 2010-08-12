67be049a45f87962665085bb3b407c1598322077
Paul Hieromnimon <paul.hieromnimon@gmail.com>
2010-08-12 15:08:19 -0700

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'rake'
+require 'echoe'
+
+Echoe.new('alter-ego-active-record', '0.1.0') do |p|
+  p.description = "Adapter to allow ActiveRecord to persist state of objects using the AlterEgo state machine"
+  p.author = "Paul Hieromnimon"
+  p.email = "paul.hieromnimon@gmail.com"
+end
---
+++ lib/alter_ego/active_record_adapter.rb
@@ -0,0 +1,30 @@
+module AlterEgo
+  module ActiveRecordAdapter
+
+    def self.included klass
+      klass.after_initialize do |r|
+        r.state = r.state
+      end
+    end
+
+    # rails needs this method to be defined for the
+    # class method after_find to be able to take a block
+    # this is because of some performance optimization
+    def after_initialize; end
+
+    # Override methods from AlterEgo to store state
+    # in @attributes["state"] instead of @state
+    def state=(identifier)
+      # state needs to always be stored as a string - symbol serialization is strange
+      @attributes["state"] = identifier.to_s unless identifier.nil?
+    end
+
+    def state
+      result = ( @attributes["state"] || self.class.default_state)
+      result = result.to_sym unless result.nil?
+      assert(result.nil? || self.class.states.keys.include?(result))
+      result
+    end
+
+  end
+end
